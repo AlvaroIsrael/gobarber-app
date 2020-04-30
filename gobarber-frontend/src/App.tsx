6bc88fa2cc92175ded92ecf96dfbc3542b47aeec
Alvaro Israel <israel.nunes.leite@gmail.com>
2020-04-30 16:54:36 -0300

    chore: Added global styles.

--- gobarber-frontend/src/App.tsx
+++ gobarber-frontend/src/App.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
 
-const App: React.FC = () => <h1>Hello World</h1>;
+const App: React.FC = () => (
+  <>
+    <h1>Hello World</h1>
+    <GlobalStyle />
+  </>
+);
 
 export default App;
