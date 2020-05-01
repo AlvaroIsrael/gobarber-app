48280d7dbe2c09c58ceff71d95c2003c0a7c4afa
Alvaro Israel <israel.nunes.leite@gmail.com>
2020-05-01 00:56:36 -0300

    chore: Implemented SignIn page with styles.

--- gobarber-frontend/src/pages/SignIn/styles.ts
+++ gobarber-frontend/src/pages/SignIn/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import {shade} from 'polished';
 import signInBackground from '../../assets/sign-in-background.png';
 
 export const Container = styled.div`
@@ -9,10 +10,84 @@ export const Container = styled.div`
 
 export const Content = styled.div`
   display: flex;
+  align-items: center;
   flex-direction: column;
   place-content: center;
   width: 100%;
   max-width: 700px;
+
+  form {
+    margin: 80px 0;
+    width: 340px;
+    text-align: center;
+
+    h1 {
+      margin-bottom: 24px;
+    }
+
+    input {
+      background: #808080;
+      border-radius: 10px;
+      border: 2px solid #232129;
+      padding: 16px;
+      width: 100%;
+      color: #262626;
+
+      &::placeholder {
+        color: #262626;
+      }
+
+      & + input {
+        margin-top: 8px;
+      }
+    }
+
+    button {
+      height: 56px;
+      background: #0f43a9;
+      border-radius: 10px;
+      border: 0;
+      padding: 0 16px;
+      color: #fff;
+      width: 100%;
+      font-weight: 500;
+      margin-top: 16px;
+      transition: background-color 0.2s;
+
+      &:hover {
+        background: ${shade(0.2, '#0f43a9')};
+      }
+    }
+
+    a {
+      color: #fff;
+      display: block;
+      margin-top: 24px;
+      text-decoration: none;
+      transition: color 0.2s;
+
+      &:hover {
+        color: ${shade(0.2, '#fff')};
+      }
+    }
+  }
+
+  > a {
+    color: #0f43a9;
+    margin-top: 24px;
+    text-decoration: none;
+    transition: color 0.2s;
+    display: flex;
+    align-items: center;
+
+    &:hover {
+      color: ${shade(0.2, '#0f43a9')};
+    }
+
+    svg {
+      margin-right: 16px;
+    }
+  }
 `;
 
 export const Background = styled.div`
