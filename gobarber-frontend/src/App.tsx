147961b41da9c73e8de21f0e04923167d8e6e2c8
Alvaro Israel <israel.nunes.leite@gmail.com>
2020-04-30 23:32:18 -0300

    chore: Initial ts files config and SignIn page.

--- gobarber-frontend/src/App.tsx
+++ gobarber-frontend/src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
+import SignIn from './pages/SignIn';
 
 const App: React.FC = () => (
   <>
-    <h1>Hello World</h1>
+    <SignIn />
     <GlobalStyle />
   </>
 );
