79c91eeaab19c3e89195ceb4a8fe57ff5c2b0850
Alvaro Israel <israel.nunes.leite@gmail.com>
2020-05-09 23:43:49 -0300

    chore: Added users model.

--- gobarber-backend/src/models/User.ts
+++ gobarber-backend/src/models/User.ts
@@ -1,15 +1,18 @@
 import {Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn} from 'typeorm';
 
-@Entity('appointments')
-class Appointment {
+@Entity('users')
+class User {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Column()
-  provider: string;
+  name: string;
 
-  @Column('timestamp with time zone')
-  date: Date;
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
 
   @CreateDateColumn()
   created_at: Date;
@@ -18,4 +21,4 @@ class Appointment {
   updated_at: Date;
 }
 
-export default Appointment;
+export default User;
