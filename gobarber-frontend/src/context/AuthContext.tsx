1668b4c4a2f5a21254bbc2f9fd0862af28b4766b
Alvaro Israel <israel.nunes.leite@gmail.com>
2020-06-21 21:38:21 -0300

    chore: Implemented logic to save context into localstorage.

--- gobarber-frontend/src/context/AuthContext.tsx
+++ gobarber-frontend/src/context/AuthContext.tsx
@@ -1,5 +1,10 @@
-import React, {createContext, PropsWithChildren, ReactNode, useCallback} from 'react';
+import React, {createContext, PropsWithChildren, ReactNode, useCallback, useState} from 'react';
 import api from '../services/api';
+
+interface AuthState {
+  token: string;
+  user: object;
+}
 
 interface SignInCredentials {
   email: string;
@@ -7,22 +12,39 @@ interface SignInCredentials {
 }
 
 interface AuthContextState {
-  name: string;
+  user: object;
 
   signIn(credentials: SignInCredentials): Promise<void>;
 }
 
 export const AuthContext = createContext<AuthContextState>({} as AuthContextState);
 
+const getDataFromLocalStorage = (): AuthState => {
+  const token = localStorage.getItem('@GoBarber:token');
+  const user = localStorage.getItem('@GoBarber:user');
+
+  if (user && token) {
+    return {token, user: JSON.parse(user)};
+  }
+
+  return {} as AuthState;
+};
+
 export const AuthProvider: React.FC = ({children}: PropsWithChildren<ReactNode>) => {
+  const [data, setData] = useState<AuthState>(getDataFromLocalStorage);
   const signIn = useCallback(async ({email, password}: SignInCredentials) => {
     try {
-      const response = await api.post('sessions', {email, password});
-      console.log(response.data);
+      const response = await api.post<AuthState>('sessions', {email, password});
+      const {token, user} = response.data;
+
+      localStorage.setItem('@GoBarber:token', token);
+      localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+      setData({token, user});
     } catch (e) {
-      console.log(e);
+      e.toString();
     }
   }, []);
 
-  return <AuthContext.Provider value={{name: 'Alvaro Israel', signIn}}>{children}</AuthContext.Provider>;
+  return <AuthContext.Provider value={{user: data.user, signIn}}>{children}</AuthContext.Provider>;
 };
