763877674e18c308aef8bc83c26144d25610547d
Alvaro Israel Nunes Leite <israel.nunes.leite@gmail.com>
2020-12-18 21:36:58 -0300

    chore: Configured basic routing for the app.

--- gobarber-frontend/src/App.tsx
+++ gobarber-frontend/src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import AppProvider from './hooks/index';
 import GlobalStyle from './styles/global';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
+import Routes from './routes';
 
 const App: React.FC = () => (
-  <>
+  <Router>
     <AppProvider>
-      <SignIn />
+      <Routes />
     </AppProvider>
     <GlobalStyle />
-  </>
+  </Router>
 );
 
 export default App;
--- gobarber-frontend/src/pages/SignIn/index.tsx
+++ gobarber-frontend/src/pages/SignIn/index.tsx
@@ -1,15 +1,16 @@
-import React, {useCallback, useRef} from 'react';
-import {FiLogIn, FiMail, FiLock} from 'react-icons/fi';
-import {Form} from '@unform/web';
-import {FormHandles} from '@unform/core';
+import React, { useCallback, useRef } from 'react';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
-import {Container, Content, Background} from './styles';
+import { Link } from 'react-router-dom';
+import { Container, Content, AnimationContainer, Background } from './styles';
 import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import getValidationErrors from '../../utils/getValidationErrors';
-import {useAuth} from '../../hooks/auth';
-import {useToast} from '../../hooks/toast';
+import { useAuth } from '../../hooks/auth';
+import { useToast } from '../../hooks/toast';
 
 interface SignInFormData {
   email: string;
@@ -19,8 +20,8 @@ interface SignInFormData {
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const {signIn} = useAuth();
-  const {addToast} = useToast();
+  const { signIn } = useAuth();
+  const { addToast } = useToast();
 
   const handleSubmit = useCallback(
     async (data: SignInFormData) => {
@@ -36,7 +37,7 @@ const SignIn: React.FC = () => {
           password: Yup.string().required('Senha obrigatória'),
         });
 
-        await schema.validate(data, {abortEarly: false});
+        await schema.validate(data, { abortEarly: false });
 
         await signIn({
           email: data.email,
@@ -46,9 +47,10 @@ const SignIn: React.FC = () => {
         if (err instanceof Yup.ValidationError) {
           const error = getValidationErrors(err);
           form.setErrors(error);
+          return;
         }
 
-        const toast = addToast({
+        addToast({
           type: 'error',
           title: 'Erro na autenticação',
           description: 'Erro ao fazer login, cheque as credenciais.',
@@ -61,21 +63,23 @@ const SignIn: React.FC = () => {
   return (
     <Container>
       <Content>
-        <img src={logoImg} alt="GoBarber" />
-        <Form ref={formRef} onSubmit={handleSubmit}>
-          <h1>Faça seu logon</h1>
+        <AnimationContainer>
+          <img src={logoImg} alt="GoBarber" />
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Faça seu logon</h1>
 
-          <Input name="email" icon={FiMail} placeholder="E-mail" type="email" />
-          <Input name="password" icon={FiLock} placeholder="Senha" type="password" />
-          <Button type="submit">Entrar</Button>
+            <Input name="email" icon={FiMail} placeholder="E-mail" type="email" />
+            <Input name="password" icon={FiLock} placeholder="Senha" type="password" />
+            <Button type="submit">Entrar</Button>
 
-          <a href="forgot">Esqueci minha senha</a>
-        </Form>
+            <a href="forgot">Esqueci minha senha</a>
+          </Form>
 
-        <a href="login">
-          <FiLogIn />
-          Criar conta
-        </a>
+          <Link to="/signup">
+            <FiLogIn />
+            Criar conta
+          </Link>
+        </AnimationContainer>
       </Content>
       <Background />
     </Container>
