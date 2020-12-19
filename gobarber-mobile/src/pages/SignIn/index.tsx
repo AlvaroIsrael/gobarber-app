cd03e3a699fe2ab8001dfbc74cc2686c36702009
Alvaro Israel Nunes Leite <israel.nunes.leite@gmail.com>
2020-12-19 15:28:37 -0300

    chore: Added font files to the project.

--- gobarber-mobile/src/pages/SignIn/index.tsx
+++ gobarber-mobile/src/pages/SignIn/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Image } from 'react-native';
-import { Container } from './styles';
+import { Container, Title } from './styles';
 import logoImage from '../../assets/logo.png';
 
 const SignIn: React.FC = () => {
   return (
     <Container>
       <Image source={logoImage} />
+      <Title>Faça seu logon</Title>
     </Container>
   );
 };
