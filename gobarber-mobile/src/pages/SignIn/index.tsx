101d0ea9da11dca32b0640fb480c907e11bffcaa
Alvaro Israel Nunes Leite <israel.nunes.leite@gmail.com>
2020-12-19 19:02:40 -0300

    chore: Changed default input cursor color. + Added scroll behavior to sign in screen . + Fixed bottom size behavior on ios.

--- gobarber-mobile/src/pages/SignIn/index.tsx
+++ gobarber-mobile/src/pages/SignIn/index.tsx
@@ -1,26 +1,51 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { Image, View, KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
 import logger from 'debug';
+import Icon from 'react-native-vector-icons/Feather';
 import logoImage from '../../assets/logo.png';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImage} />
-      <Title>Faça seu logon</Title>
-      <Input name={'email'} icon={'mail'} placeholder={'E-mail'} />
-      <Input name={'password'} icon={'lock'} placeholder={'Senha'} />
-      <Button
-        onPress={() => {
-          logger.log('deu!');
-        }}>
-        Entrar
-      </Button>
-    </Container>
+    <>
+      <KeyboardAvoidingView style={{ flex: 1 }} behavior={Platform.OS === 'ios' ? 'padding' : undefined} enabled>
+        <ScrollView contentContainerStyle={{ flex: 1 }} keyboardShouldPersistTaps='handled'>
+          <Container>
+            <Image source={logoImage} />
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
+            <Input name={'email'} icon={'mail'} placeholder={'E-mail'} />
+            <Input name={'password'} icon={'lock'} placeholder={'Senha'} />
+            <Button
+              onPress={() => {
+                logger.log('deu!');
+              }}>
+              Entrar
+            </Button>
+            <ForgotPassword onPress={() => {
+            }}>
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <CreateAccountButton>
+        <Icon name='log-in' size={20} color='#fff' />
+        <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
   );
 };
 
