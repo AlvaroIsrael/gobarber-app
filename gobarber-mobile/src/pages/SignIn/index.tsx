1bd232d449018fac33964557919369224d692cbb
Alvaro Israel Nunes Leite <israel.nunes.leite@gmail.com>
2020-12-19 13:30:38 -0300

    chore: Added logo to the project.

--- gobarber-mobile/src/pages/SignIn/index.tsx
+++ gobarber-mobile/src/pages/SignIn/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import { Image } from 'react-native';
 import { Container } from './styles';
+import logoImage from '../../assets/logo.png';
 
 const SignIn: React.FC = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Image source={logoImage} />
+    </Container>
+  );
 };
 
 export default SignIn;
