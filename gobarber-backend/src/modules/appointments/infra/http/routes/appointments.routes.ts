b2cd963fb0540126c35d6c4405e153a569842a4a
Alvaro Israel Nunes Leite <israel.nunes.leite@gmail.com>
2021-01-25 14:10:56 -0300

    chore: Dependency inversion principle applied to the code.

--- gobarber-backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ gobarber-backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,5 +1,4 @@
 import { Router } from 'express';
-import { getCustomRepository } from 'typeorm';
 import { parseISO } from 'date-fns';
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
@@ -7,11 +6,11 @@ import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAut
 import * as HttpStatus from 'http-status-codes';
 
 const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
 
 appointmentsRouter.use(ensureAuthenticated);
 
 appointmentsRouter.get('/', async (request, response) => {
-  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
   const appointments = await appointmentsRepository.find();
   return response.json(appointments);
 });
@@ -19,7 +18,7 @@ appointmentsRouter.get('/', async (request, response) => {
 appointmentsRouter.post('/', async (request, response) => {
   const { providerId, date } = request.body;
   const parsedDate = parseISO(date);
-  const createAppointment = new CreateAppointmentService();
+  const createAppointment = new CreateAppointmentService(appointmentsRepository);
 
   const appointment = await createAppointment.execute({
     date: parsedDate,
