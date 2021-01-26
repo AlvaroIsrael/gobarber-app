b9795da6a2e3ce9b292ec3753b9399525f07d683
Alvaro Israel Nunes Leite <israel.nunes.leite@gmail.com>
2021-01-26 13:52:37 -0300

    chore: Added StorageProvider to the project.

--- gobarber-backend/src/config/upload.ts
+++ gobarber-backend/src/config/upload.ts
@@ -6,6 +6,7 @@ const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
 export default {
   directory: tmpFolder,
+  uploadsFolder: path.resolve(tmpFolder, 'uploads'),
   storage: multer.diskStorage({
     destination: tmpFolder,
     filename(request, file, callback) {
