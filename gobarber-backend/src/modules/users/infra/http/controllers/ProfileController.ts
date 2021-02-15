38254f23079dcb24b9f2eb0653278642561bbadc
Alvaro Israel Nunes Leite <israel.nunes.leite@gmail.com>
2021-02-15 16:37:05 -0300

    chore: Added update profile controller.

---
+++ gobarber-backend/src/modules/users/infra/http/controllers/ProfileController.ts
@@ -0,0 +1,54 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import UpdateProfileService from '@modules/users/services/UpdateProfileService';
+import ShowProfileService from '@modules/users/services/ShowProfileService';
+
+class ProfileController {
+  public async show(request: Request, response: Response): Promise<Response> {
+    const user_id = request.user.id;
+
+    const showProfile = container.resolve(ShowProfileService);
+
+    const user = await showProfile.execute({
+      user_id,
+    });
+
+    const userWithoutPassword = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
+
+    return response.json(userWithoutPassword);
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const user_id = request.user.id;
+    const { name, email, old_password, password } = request.body;
+
+    const updateProfile = container.resolve(UpdateProfileService);
+
+    const user = await updateProfile.execute({
+      user_id,
+      name,
+      email,
+      old_password,
+      password,
+    });
+
+    const userWithoutPassword = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
+
+    return response.json(userWithoutPassword);
+  }
+}
+
+export default ProfileController;
--- gobarber-backend/src/shared/infra/http/routes/index.ts
+++ gobarber-backend/src/shared/infra/http/routes/index.ts
@@ -3,6 +3,7 @@ import appointmentsRouter from '@modules/appointments/infra/http/routes/appointm
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import passwordRouter from '@modules/users/infra/http/routes/password.routes';
+import profileRouter from '@modules/users/infra/http/routes/profile.routes';
 import uploadConfig from '@config/upload';
 
 const routes = Router();
@@ -12,5 +13,6 @@ routes.use('/api/v1/appointments', appointmentsRouter);
 routes.use('/api/v1/users', usersRouter);
 routes.use('/api/v1/sessions', sessionsRouter);
 routes.use('/api/v1/password', passwordRouter);
+routes.use('/api/v1/profile', profileRouter);
 
 export default routes;
