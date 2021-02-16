0960a7a0f1e2fc37488735aea5ab04c1ceeb78a5
Alvaro Israel Nunes Leite <israel.nunes.leite@gmail.com>
2021-02-15 22:09:28 -0300

    fix: Fixed typos in CreateAppointmentService.

--- gobarber-backend/src/modules/appointments/services/CreateAppointmentService.ts
+++ gobarber-backend/src/modules/appointments/services/CreateAppointmentService.ts
@@ -31,7 +31,7 @@ class CreateAppointmentService {
     }
 
     if (getHours(appointmentDate) < 8 || getHours(appointmentDate) > 17) {
-      throw new AppError('You can only create appontments between 8am and 5pm.',
+      throw new AppError('You can only create appointments between 8am and 5pm.',
       );
     }
 
