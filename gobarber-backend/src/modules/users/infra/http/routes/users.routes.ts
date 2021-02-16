ccadc25ae716a4c380ee8f6ad19cc393eb56f4af
Alvaro Israel Nunes Leite <israel.nunes.leite@gmail.com>
2021-02-16 15:31:49 -0300

    chore: Added data validation middlewares for every sensitive route that requires it.

--- gobarber-backend/src/modules/users/infra/http/routes/users.routes.ts
+++ gobarber-backend/src/modules/users/infra/http/routes/users.routes.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
+
 import UsersController from '../controllers/UsersController';
 import UsersAvatarsController from '../controllers/UsersAvatarsController';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
@@ -10,7 +12,14 @@ const upload = multer(uploadConfig);
 const usersController = new UsersController();
 const usersAvatarsController = new UsersAvatarsController();
 
-usersRouter.post('/', usersController.create);
+usersRouter.post('/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }), usersController.create);
 
 usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), usersAvatarsController.update);
 
