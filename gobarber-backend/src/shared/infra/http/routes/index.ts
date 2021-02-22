8a64c5cdfcb5eb4e85cae96155883d181e0613ce
Alvaro Israel Nunes Leite <israel.nunes.leite@gmail.com>
2021-02-22 19:55:59 -0300

    fix: Added swagger to the app. + Documented sessions route.

--- gobarber-backend/src/shared/infra/http/routes/index.ts
+++ gobarber-backend/src/shared/infra/http/routes/index.ts
@@ -6,8 +6,11 @@ import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import passwordRouter from '@modules/users/infra/http/routes/password.routes';
 import profileRouter from '@modules/users/infra/http/routes/profile.routes';
 import uploadConfig from '@config/upload';
+import swaggerUi from 'swagger-ui-express';
+import yaml from 'yamljs';
 
 const routes = Router();
+const swagger = yaml.load('./swagger.yaml');
 
 routes.use('/api/v1/files', express.static(uploadConfig.uploadsFolder));
 routes.use('/api/v1/appointments', appointmentsRouter);
@@ -16,5 +19,6 @@ routes.use('/api/v1/users', usersRouter);
 routes.use('/api/v1/sessions', sessionsRouter);
 routes.use('/api/v1/password', passwordRouter);
 routes.use('/api/v1/profile', profileRouter);
+routes.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swagger));
 
 export default routes;
