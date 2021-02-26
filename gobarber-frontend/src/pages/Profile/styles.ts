e0bd4bbe13146014411533f5b8bd62c35c626528
Alvaro Israel Nunes Leite <israel.nunes.leite@gmail.com>
2021-02-25 21:57:36 -0300

    chore: Added user profile page.

---
+++ gobarber-frontend/src/pages/Profile/styles.ts
@@ -0,0 +1,107 @@
+import styled from 'styled-components';
+import { shade } from 'polished';
+
+export const Container = styled.div`
+  > header {
+    height: 144px;
+    background: #393939;
+    display: flex;
+    align-items: center;
+
+    div {
+      width: 100%;
+      max-width: 1120px;
+      margin: 0 auto;
+
+      svg {
+        color: #999591;
+        width: 24px;
+        height: 24px;
+      }
+    }
+  }
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: -176px auto 0;
+  width: 100%;
+
+  form {
+    margin: 80px 0;
+    width: 340px;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+
+    input:-webkit-autofill,
+    input:-webkit-autofill:hover,
+    input:-webkit-autofill:focus,
+    input:-webkit-autofill:active {
+      -webkit-box-shadow: 0 0 0 30px #808080 inset !important;
+    }
+
+    h1 {
+      margin-bottom: 24px;
+      font-size: 36px;
+      font-family: Montserrat, sans-serif;
+      font-weight: bolder;
+      text-align: center;
+    }
+  }
+
+  a {
+    color: #f4ede8;
+    display: block;
+    margin-top: 24px;
+    text-decoration: none;
+    transition: color 0.2s;
+
+    &:hover {
+      color: ${shade(0.2, '#ffffff')};
+    }
+  }
+`;
+
+export const AvatarInput = styled.div`
+  margin-bottom: 32px;
+  position: relative;
+  align-self: center;
+
+  img {
+    width: 186px;
+    height: 186px;
+    border-radius: 50%;
+  }
+
+  button {
+    position: absolute;
+    width: 48px;
+    height: 48px;
+    color: #ffffff;
+    border-radius: 50%;
+    right: 0;
+    bottom: 0;
+    border: 0;
+    transition: background-color 0.2s;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: #808080;
+
+    svg {
+      width: 20px;
+      height: 20px;
+      color: #393939;
+    }
+
+    &:hover {
+      svg {
+        color: #ffffff;
+      }
+    }
+  }
+`;
