4e0c78dd271f9eda703817b653adad0543bb77ba
Alvaro Israel Nunes Leite <israel.nunes.leite@gmail.com>
2021-02-27 20:49:18 -0300

    feat: Added frontend tests to the application.

---
+++ gobarber-frontend/src/__tests__/components/input.spec.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+import { render, fireEvent, waitFor } from '@testing-library/react';
+import Input from '../../components/Input';
+
+jest.mock('@unform/core', () => {
+  return {
+    useField() {
+      return {
+        fieldName: 'email',
+        defaultValue: '',
+        error: '',
+        registerField: jest.fn(),
+      };
+    },
+  };
+});
+
+describe('Input component', () => {
+  it('should be able to render an input', () => {
+    const { getByPlaceholderText } = render(<Input name="email" placeholder="E-mail" />);
+
+    expect(getByPlaceholderText('E-mail')).toBeTruthy();
+  });
+
+  it('should render highlight on input focus', async () => {
+    const { getByPlaceholderText, getByTestId } = render(<Input name="email" placeholder="E-mail" />);
+
+    const inputElement = getByPlaceholderText('E-mail');
+    const containerElement = getByTestId('input-container');
+
+    fireEvent.focus(inputElement);
+
+    await waitFor(() => {
+      expect(containerElement).toHaveStyle('border-color: #fff;');
+      expect(containerElement).toHaveStyle('color: #fff;');
+    });
+
+    fireEvent.blur(inputElement);
+
+    await waitFor(() => {
+      expect(containerElement).not.toHaveStyle('border-color: #fff;');
+      expect(containerElement).not.toHaveStyle('color: #fff;');
+    });
+  });
+
+  it('should keep input border highlight when input filled', async () => {
+    const { getByPlaceholderText, getByTestId } = render(<Input name="email" placeholder="E-mail" />);
+
+    const inputElement = getByPlaceholderText('E-mail');
+    const containerElement = getByTestId('input-container');
+
+    fireEvent.change(inputElement, {
+      target: { value: 'johndoe@example.com.br' },
+    });
+
+    fireEvent.blur(inputElement);
+
+    await waitFor(() => {
+      expect(containerElement).toHaveStyle('color: #fff;');
+    });
+  });
+});
