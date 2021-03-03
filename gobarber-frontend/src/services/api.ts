f8367fdf6d305d90d947414b58f078108dfa0678
Alvaro Israel Nunes Leite <israel.nunes.leite@gmail.com>
2021-03-03 16:51:29 -0300

    fix: Configured app for production.

--- gobarber-frontend/src/services/api.ts
+++ gobarber-frontend/src/services/api.ts
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: 'http://localhost:3333',
-});
+const api = axios.create({ baseURL: process.env.REACT_APP_API_URL });
 
 export default api;
