a1183449963b1a4750257069db720f09fae50160
Alvaro Israel Nunes Leite <israel.nunes.leite@gmail.com>
2021-03-05 14:47:37 -0300

    feat: Fetching providers from api.

--- gobarber-mobile/src/pages/Dashboard/index.tsx
+++ gobarber-mobile/src/pages/Dashboard/index.tsx
@@ -1,15 +1,41 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { useAuth } from '../../hooks/auth';
-import { Container, Header, HeaderTitle, UserName, ProfileButton, UserAvatar } from './styles';
+import { Container, Header, HeaderTitle, UserName, ProfileButton, UserAvatar, ProvidersList } from './styles';
+import api from '../../services/api';
+
+export interface Provider {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
 
 const Dashboard: React.FC = () => {
   const { signOut, user } = useAuth();
-  const { navigate } = useNavigation();
+  const navigation = useNavigation();
+  const [providers, setProviders] = useState<Provider[]>([]);
 
   const navigateToProfile = useCallback(() => {
-    navigate('Profile');
-  }, [navigate]);
+    /* navigation.navigate('Profile'); */
+    signOut();
+  }, [navigation, signOut]);
+
+  useEffect(() => {
+    api
+      .get('/api/v1/providers')
+      .then(response => {
+        console.log(response);
+        setProviders(response.data);
+      })
+      .catch(() => setProviders([]));
+  }, []);
+
+  const handleSelectProvider = useCallback(
+    (providerId: string) => {
+      navigation.navigate('AppointmentDatePicker', { providerId });
+    },
+    [navigation],
+  );
 
   return (
     <Container>
@@ -22,6 +48,12 @@ const Dashboard: React.FC = () => {
         <ProfileButton onPress={navigateToProfile}>
           <UserAvatar source={{ uri: user.avatar_url }} />
         </ProfileButton>
+
+        <ProvidersList
+          keyExtractor={provider => provider.id}
+          data={providers}
+          renderItem={({ item }) => <UserName>{item.name}</UserName>}
+        />
       </Header>
     </Container>
   );
