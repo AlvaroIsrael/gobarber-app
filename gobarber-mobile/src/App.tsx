c003f5f1ffe0d038c559e389f07c6975e04a5c69
AlvaroIsrael <israel.nunes.leite@gmail.com>
2021-08-09 19:48:41 -0300

    feat(👑): Added android splash screen to the app.

--- gobarber-mobile/src/App.tsx
+++ gobarber-mobile/src/App.tsx
@@ -1,19 +1,27 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import SplashScreen from 'react-native-splash-screen';
 import AppProvider from './hooks';
 import Routes from './routes';
 
-const App: React.FC = () => (
-  <NavigationContainer>
-    <StatusBar barStyle='light-content' backgroundColor='#262626' translucent />
-    <AppProvider>
-      <View style={{ flex: 1, backgroundColor: '#262626' }}>
-        <Routes />
-      </View>
-    </AppProvider>
-  </NavigationContainer>
-);
+const App: React.FC = () => {
+  useEffect(() => {
+    // verify if user is logged in.
+    SplashScreen.hide();
+  }, []);
+
+  return (
+    <NavigationContainer>
+      <StatusBar barStyle='light-content' backgroundColor='#262626' translucent />
+      <AppProvider>
+        <View style={{ flex: 1, backgroundColor: '#262626' }}>
+          <Routes />
+        </View>
+      </AppProvider>
+    </NavigationContainer>
+  );
+};
 
 export default App;
