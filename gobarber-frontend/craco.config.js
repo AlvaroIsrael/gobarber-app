e4a9f02e08d09ae7f40e606e91f76477c9bb2302
AlvaroIsrael <israel.nunes.leite@gmail.com>
2021-08-24 02:00:51 -0300

    fix(🐛): Fixed esbuild-loader in configuration file.

--- gobarber-frontend/craco.config.js
+++ gobarber-frontend/craco.config.js
@@ -1,23 +1,61 @@
-const CracoEsbuildPlugin = require('craco-esbuild');
+const { addAfterLoader, removeLoaders, loaderByName, getLoaders, throwUnexpectedConfigError } = require('@craco/craco');
+const { ESBuildMinifyPlugin } = require('esbuild-loader');
+
+const throwError = message =>
+  throwUnexpectedConfigError({
+    packageName: 'craco',
+    githubRepo: 'gsoft-inc/craco',
+    message,
+    githubIssueQuery: 'webpack',
+  });
 
 module.exports = {
-  plugins: [
-    {
-      plugin: CracoEsbuildPlugin,
-      options: {
-        enableSvgr: true,
-        esbuildLoaderOptions: {
+  webpack: {
+    configure: (webpackConfig, { paths }) => {
+      const { hasFoundAny, matches } = getLoaders(webpackConfig, loaderByName('babel-loader'));
+      if (!hasFoundAny) {
+        throwError('failed to find babel-loader');
+      }
+
+      const { hasRemovedAny, removedCount } = removeLoaders(webpackConfig, loaderByName('babel-loader'));
+      if (!hasRemovedAny) {
+        throwError('no babel-loader to remove');
+      }
+      if (removedCount !== 2) {
+        throwError('had expected to remove 2 babel loader instances');
+      }
+
+      const tsLoader = {
+        test: /\.(js|mjs|jsx|ts|tsx)$/,
+        include: paths.appSrc,
+        loader: require.resolve('esbuild-loader'),
+        options: {
           loader: 'tsx',
           target: 'es2015',
         },
-        skipEsbuildJest: false,
-        esbuildJestOptions: {
-          loaders: {
-            '.ts': 'ts',
-            '.tsx': 'tsx',
-          },
-        },
-      },
+      };
+
+      const { isAdded: tsLoaderIsAdded } = addAfterLoader(webpackConfig, loaderByName('url-loader'), tsLoader);
+      if (!tsLoaderIsAdded) {
+        throwError('failed to add esbuild-loader');
+      }
+
+      const { isAdded: babelLoaderIsAdded } = addAfterLoader(
+        webpackConfig,
+        loaderByName('esbuild-loader'),
+        matches[1].loader, // babel-loader
+      );
+      if (!babelLoaderIsAdded) {
+        throwError('failed to add back babel-loader for non-application JS');
+      }
+
+      webpackConfig.optimization.minimizer = [
+        new ESBuildMinifyPlugin({
+          target: 'es2015',
+        }),
+      ];
+
+      return webpackConfig;
     },
-  ],
+  },
 };
