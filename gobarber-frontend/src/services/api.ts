430621fdef2aad2324819ebbf6637e0f0c3047b9
AlvaroIsrael <israel.nunes.leite@gmail.com>
2023-03-15 00:04:18 -0300

    feat(👑): Adds prefix VITE_ to enviroment variable.

--- gobarber-frontend/src/services/api.ts
+++ gobarber-frontend/src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: import.meta.env.REACT_APP_API_URL });
+const api = axios.create({ baseURL: import.meta.env.VITE_REACT_APP_API_URL });
 
 export default api;
